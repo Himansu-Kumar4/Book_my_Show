83ef67887aee2e9ab0c27674d43acbc34cd64132
Himansu Kumar <kumarhimanshu172949@gmail.com>
2026-06-11 21:48:15 +0530

    with slider & container

--- BMS.js
+++ BMS.js
@@ -1,3 +1,40 @@
+let slides=document.querySelector(".slides")
+let images=document.querySelectorAll(".slides img")
+let prevBtn=document.querySelector(".prev")
+let nextBtn=document.querySelector(".next")
+
+let index=0
+
+function showSlide(){
+    slides.style.transform=`translateX(-${index * 100}%)`
+}
+
+prevBtn.addEventListener("click",()=>{
+    index--;
+    if(index<0){
+        index=images.length-1;
+    }
+    showSlide();
+})
+
+
+nextBtn.addEventListener("click",()=>{
+    index++;
+    console.log("clicked")
+    if(index>=images.length){
+        index=0
+    }
+    showSlide();
+})
+
+setInterval(()=>{
+    index++;
+    if(index>=images.length){
+        index=0;
+    }
+    showSlide();
+},4000)
+
 const searchvalue = document.querySelector("#searchinput");
 const searchbtn = document.querySelector("#searchbtn");
 
@@ -5,12 +42,13 @@ searchbtn.addEventListener("click", searchlist);
 
 function searchlist() {
     let Searchdata = searchvalue.value.trim().toLowerCase();
-    if (Searchdata === "") {
+    if (Searchdata == "") {
         alert("Please enter what to search for");
         return;
     }
+
     // Get all list items
-   let items = document.querySelectorAll("#fullbody *");
+    let items = document.querySelectorAll("#fullbody .the");
 
     items.forEach((element) => {
         if (element.innerText.toLowerCase().includes(Searchdata)) {
@@ -20,3 +58,36 @@ function searchlist() {
         }
     });
 }
+
+//searchvalue.addEventListener("input", searchlist2);
+
+//this is a debounce function that will delay the search request
+function debounce(func, delay){
+    let timer;
+    return function(...arg){ //this is a spread operator
+        clearTimeout(timer);
+        timer=setTimeout(()=>{
+            func(...arg)
+        },delay)
+    }
+}
+
+let Searchdata = searchvalue.value.trim().toLowerCase();
+
+function searchlist2(Searchdata) {
+    // Get all list items
+    let items = document.querySelectorAll("#fullbody .the");
+
+    items.forEach((element) => {
+        if (element.innerText.toLowerCase().includes(Searchdata)) {
+            element.style.display = "";   // show matching
+        } else {
+            element.style.display = "none"; // hide non-matching
+        }
+    });
+}
+
+let debouncesearch=debounce(searchlist2,3000)
+searchvalue.addEventListener("input",(e)=>{
+    debouncesearch(e.target.value);
+})
